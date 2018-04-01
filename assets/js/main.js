708c4b4285a079b462feb3d526775645be47a2d9
Zac Barnaby <zacbarnaby@zacs-mac-pro.home>
2018-04-01 14:13:17 +0100

    First commit

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+});
+
+$('#typed').typeIt({
+  strings: ["I'm <b>Zac</b>.", "I'm a <b>front-end web developer</b>."],
+  breakLines: false,
+  speed: 120,
+  loop: true,
+  loopDelay: 2000,
+  deleteSpeed: 100
+})
