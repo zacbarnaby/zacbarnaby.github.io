a05de59fcc7859046e8e719c9df8ef0e2f103855
Zac Barnaby <zacbarnaby@zacs-mac-pro.home>
2018-04-01 14:28:01 +0100

    Updating portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,59 @@
-$(document).ready(function() {
+$( document ).ready(function() {
+  let header = $("header").offset().top;
+  let headerHeight = $("header").outerHeight();
+
+  $(window).scroll(() => {
+    let windowSize = $(window).width();
+    if(windowSize > 805) {
+      if($(window).scrollTop() > 20) {
+        $("header").addClass('fixed');
+      } else {
+        $("header").removeClass('fixed');
+      }
+    }
+
+  });
+
+  $(".progress-inner").each((i, v) => {
+    let percent = $(v).data("percent");
+    $(v).animate({
+      width: percent + "%"
+    }, 1000);
+  });
+
+  $('ul#nav li a').click((e) => {
+    e.preventDefault();
+    let id = $(e.target).data('nav');
+    navGoTo(id);
+  });
+
+  $('ul#types a').on('click', function(e) {
+    e.preventDefault();
+    let type = $(this).data('type');
+    if(type == "education") {
+      $('ul#types a li').removeClass('active');
+      $(this).find('li').addClass('active');
+      $("#education").addClass('active');
+      $("#experience").removeClass('active');
+    } else if (type == "experience") {
+      $('ul#types a li').removeClass('active');
+      $(this).find('li').addClass('active');
+      $("#education").removeClass('active');
+      $("#experience").addClass('active');
+    }
+  });
+
+  $('.hamburger').click(function() {
+      let active = $(this).hasClass('is-active');
+      let dropdown = $('.dropdown');
+      if(!active) {
+        $(this).addClass('is-active');
+        dropdown.slideDown();
+      } else {
+        $(this).removeClass('is-active');
+        dropdown.slideUp();
+      }
+  });
 
 });
 
@@ -10,3 +65,9 @@ $('#typed').typeIt({
   loopDelay: 2000,
   deleteSpeed: 100
 })
+
+function navGoTo(element) {
+  $('html, body').animate({
+    scrollTop: $('#'+element).offset().top
+  }, 'slow');
+}
